0aa0dba3136211711ae5d3041fa9d4289e2f33a9
amirahnadzri <amirah.nadzri@gmail.com>
2022-11-09 01:34:12 +0900

    Update settings.py

--- TestServer/backendproject/settings.py
+++ TestServer/backendproject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-re4brvi-&&k0#eeo0#@4c7e86as%#ut5dr@(1lqn!2z9drk@ia'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['amirahnadzri.pythonanywhere.com','172.30.1.55', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['amirahnadzri.pythonanywhere.com']
 
 
 # Application definition
